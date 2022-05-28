0d71ddebbd23383b3ca8a376072a94fd04129c2e
Leandro Valido <leandrovalido at gmail.com>
2022-05-27 21:38:12 -0300

    Base Config

--- index.ts
+++ index.ts
@@ -20,61 +20,64 @@ app.get('/',(req, res) =>{
     res.send(appName);
 })
 
-app.post('/practice/v2/payment/gp/token',(req, res) =>{
-      var data = JSON.stringify({
-            name: req.body.name,
-            number: req.body.number,
-            cvc: req.body.cvc,
-            expirationMonth: req.body.expirationMonth,
-            expirationYear: req.body.expirationYear,
-            nickname: req.body.nickname,
-      });
+app.post('/practice/v2/payment/gp/token', (req, res) => {
 
-      var datatoken = JSON.stringify({
-        "api_key": "feU5neXF7QAkLzkdi4MJhdkE/i7bFlv3EJEP2rfp4r8"
-      });
+    console.log(JSON.stringify(req.body));
+    
+    var data = JSON.stringify({
+          name: req.body.name,
+          number: req.body.number,
+          cvc: req.body.cvc,
+          expirationMonth: req.body.expirationMonth,
+          expirationYear: req.body.expirationYear,
+          nickname: req.body.nickname,
+    });
+
+    var datatoken = JSON.stringify({
+      "api_key": "feU5neXF7QAkLzkdi4MJhdkE/i7bFlv3EJEP2rfp4r8"
+    });
       
-      var configtoken = {
+    var configtoken = {
+      method: 'post',
+      url: 'https://api.huli.io/practice/v2/authorization/token',
+      headers: { 
+        'cache-control': 'no-cache', 
+        'Content-Type': 'application/json'
+      },
+      data : datatoken
+    };
+    var token = "";
+    axios(configtoken)
+    .then(function (response) {
+      token = response.data.data.jwt;
+      var config = {
         method: 'post',
-        url: 'https://api.huli.io/practice/v2/authorization/token',
+        url: PAYMENT_GP_TOKEN_URL,
         headers: { 
-          'cache-control': 'no-cache', 
+          'id_organization': '1',//req.headers.id_organization, 
+          'Authorization': token,//req.headers.authorization, 
           'Content-Type': 'application/json'
         },
-        data : datatoken
+        data : data
       };
-      var token = "";
-      axios(configtoken)
-      .then(function (response) {
-        token = response.data.data.jwt;
-        var config = {
-          method: 'post',
-          url: PAYMENT_GP_TOKEN_URL,
-          headers: { 
-            'id_organization': '1',//req.headers.id_organization, 
-            'Authorization': token,//req.headers.authorization, 
-            'Content-Type': 'application/json'
-          },
-          data : data
-        };
 
-        axios(config)
-          .then(function (response) {
-            // console.log(req);
-            res.send(response.data);
-        })
-        .catch(function (err) {
-          var error = JSON.stringify({
-              code: "HCO00014",
-              msg: "An unexpected errors has happen"
-          });
-          res.send(error);
-          //console.log(err);
-        });      
+      axios(config)
+        .then(function (response) {
+          // console.log(req);
+          res.send(response.data);
       })
-      .catch(function (error) {
-        // console.log(error);
-      });
+      .catch(function (err) {
+        var error = JSON.stringify({
+            code: "HCO00014",
+            msg: "An unexpected errors has happen"
+        });
+        res.send(error);
+        //console.log(err);
+      });      
+    })
+    .catch(function (error) {
+      // console.log(error);
+    });
 });
  
 const port = process.env.PORT || 8080;
